e1019b8e6e675af5e9d3afc644d878f93c97177f
Oleg <hblocks99@gmail.com>
2017-09-08 11:32:14 +0300

    upload header
    upload header

---
+++ header.php
@@ -0,0 +1,22 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=<?=LANG_CHARSET;?>">
+<META NAME="ROBOTS" content="ALL">
+		<link href="Home_files/css.css" rel="stylesheet"> 
+        <link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/styles.css" type="text/css">
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/css.css" type="text/css">
+			<script src="<?=SITE_TEMPLATE_PATH?>/js/jquery-2.js"></script>
+	        <script src="<?=SITE_TEMPLATE_PATH?>/js/jquery.js"></script>
+	        <script src="<?=SITE_TEMPLATE_PATH?>/js/bootstrap.js"></script>
+	        <script src="<?=SITE_TEMPLATE_PATH?>/js/script.js"></script>
+			<script src="<?=SITE_TEMPLATE_PATH?>/js/jcarousel.js"></script>
+<?$APPLICATION->ShowMeta("keywords")?>
+<?$APPLICATION->ShowMeta("description")?>
+<title><?$APPLICATION->ShowTitle()?></title>
+<?$APPLICATION->ShowCSS();?>
+</head>
+<body>
+<?$APPLICATION->ShowPanel();?>
